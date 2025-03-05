2d9dc60d1cb6540bb775e309a4c8fb323a39b6cf
aidy <aidycodes@gmail.com>
2025-03-05 10:31:31 +0000

    added meta data

--- app/(Landing)/layout.tsx
+++ app/(Landing)/layout.tsx
@@ -1,4 +1,14 @@
 import Navbar from "../../components/Navbar";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+        /* eslint-disable-next-line react/no-unescaped-entities */
+    title: 'Aidan O\'Connor',
+    description: 'Aidan O\'Connor is a full stack web developer based in Bristol, UK.',
+    icons: {
+        icon: '/favicon.png',
+    },
+}
 
 const Layout = ({children}: {children: React.ReactNode}) => {
     return (
--- components/projects/ProjectShowCase.tsx
+++ components/projects/ProjectShowCase.tsx
@@ -12,6 +12,23 @@ export type Project = {
 
 const projects: Project[] = [
   {
+    title: 'Online Store',
+    description: `A online store with user authentication and dashboard to see past orders, SSR for products, user and cart data, intergrating stripe for payments fully
+     typesafe api using zod with jstack and modern data fetching patterns ulitzliing optimistic updates with tanstack query`,
+    image: '/previews/PosterShop.png',
+    techStack: ['React','NextJS 15', 'TailwindCSS', 'Postgres', 'Stripe', 'Better-auth', 'Hono', 'Zod', 'React Hook Form',  'Drizzle', 'TanStack'],
+    githubLink: 'https://github.com/aidycodes/postershop2025',
+    liveLink: 'https://postershop2025.vercel.app/',
+  },
+  {
+    title: 'Image Converter',
+    description: 'A NextJS 15 project that allows you to convert images to different formats and sizes ultilizing the sharp library and the features of the most recent update of NextJS',
+    image: '/previews/ImageConverter.jpg',
+    techStack: ['React','NextJS 15', 'TailwindCSS', 'Sharp', 'ShadCN', 'React Hook Form', 'TanStack Query'],
+    githubLink: 'https://github.com/aidycodes/Image-Converter',
+    liveLink: 'https://image-converter-pied-two.vercel.app/',
+  },
+  {
     title: 'Dashboard',
     description: `A full solutation for managing an online store for maintaining orders customers and editing, adding, removing new products/categories aswell as alerting to stock levels
      with charts, graphs and tables with pagination and complex filtering for the orders  and revenue and analytics related to how users interact with the site`,
@@ -19,23 +36,6 @@ const projects: Project[] = [
     techStack: ['React', 'Next.js', 'Typescript', 'Postgres', 'TailwindCSS', 'ShadCN', 'Hono', 'Clerk', 'Drizzle', 'TanStack', 'Recharts', 'zod', 'React-hook-form'],
     githubLink: 'https://github.com/aidycodes/dashboard',
     liveLink: 'https://dashboard-ebon-ten-41.vercel.app/',
-  },
-  {
-    title: 'Image Converter',
-    description: 'A NextJS 15 project that allows you to convert images to different formats and sizes ultilizing the sharp library and the features of the most recent update of NextJS',
-    image: '/previews/imageConverter.jpg',
-    techStack: ['React','NextJS 15', 'TailwindCSS', 'Sharp', 'ShadCN', 'React Hook Form', 'TanStack Query'],
-    githubLink: 'https://github.com/aidycodes/Image-Converter',
-    liveLink: 'https://image-converter-pied-two.vercel.app/',
-  },
-  {
-    title: 'Online Store',
-    description: `A online store with user authentication and dashboard to see past orders, SSR for products, user and cart data, intergrating stripe for payments fully
-     typesafe api using zod with jstack and modern data fetching patterns ulitzliing optimistic updates with tanstack query`,
-    image: '/previews/postershop.png',
-    techStack: ['React','NextJS 15', 'TailwindCSS', 'Postgres', 'Stripe', 'Better-auth', 'Hono', 'Zod', 'React Hook Form',  'Drizzle', 'TanStack'],
-    githubLink: 'https://github.com/aidycodes/postershop2025',
-    liveLink: 'https://postershop2025.vercel.app/',
   },
   {
     title: 'Snake Game',
